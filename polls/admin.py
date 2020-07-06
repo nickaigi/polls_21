67cced75cdd47c5f3b692d8a88e466d802e0fc00
Nickson Kaigi <nickaigi@gmail.com>
2020-07-06 16:40:58 +0300

    admin customization

--- polls/admin.py
+++ polls/admin.py
@@ -7,6 +7,10 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date Information', {
